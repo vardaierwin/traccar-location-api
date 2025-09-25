60b22aea688019e77ee295b7ed92fb7781930a51
vardaierwin <vardaiterwin@gmail.com>
2025-09-25 23:58:58 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,16 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
 import psycopg2
+import requests
 from dotenv import load_dotenv
 import os
 
 load_dotenv()
 
 app = FastAPI()
+
 DATABASE_URL = os.getenv("DATABASE_URL")
+TRACCAR_URL = os.getenv("TRACCAR_URL")
 
 class Location(BaseModel):
     device_id: str
@@ -16,6 +19,7 @@ class Location(BaseModel):
 
 @app.post("/locations")
 async def receive_location(loc: Location):
+    # 1. Mentés Neon DB-be
     try:
         conn = psycopg2.connect(DATABASE_URL)
         cur = conn.cursor()
@@ -28,6 +32,13 @@ async def receive_location(loc: Location):
         conn.close()
     except Exception as e:
         return {"status": "error", "message": str(e)}
+
+    # 2. Forward az ngrok URL-re
+    try:
+        payload = {"id": loc.device_id, "lat": loc.latitude, "lon": loc.longitude}
+        r = requests.get(f"{TRACCAR_URL}/?id={payload['id']}&lat={payload['lat']}&lon={payload['lon']}")
+    except Exception as e:
+        print("Traccar forward error:", e)
 
     return {"status": "ok", "device_id": loc.device_id}
 
